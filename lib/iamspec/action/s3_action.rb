e1d7a1cbbd5d259b82c64cb49457071e2dfdf997
Imri Zvik <imrizvik@gmail.com>
2018-02-08 12:47:39 +0200

    * For s3:listbucket actions - populate the s3:prefix context from the resource path

--- lib/iamspec/action/s3_action.rb
+++ lib/iamspec/action/s3_action.rb
@@ -19,6 +19,18 @@ module Iamspec::Action
       super(action_names, caller_arn, resource_arns)
     end
 
+    def with_resource(resource_arn)
+      if resource_arn.start_with?('arn:aws:s3:::') and @action_names.map(&:downcase).include?('s3:listbucket')
+        path = resource_arn.split("/")
+        if path.size > 1
+          @context_entries[:s3_prefix] = Aws::IAM::Types::ContextEntry.new({context_key_name: "s3:prefix", context_key_values: [path.join('/')], context_key_type: "string"})
+        end
+        resource_arn = path.shift
+      end
+      @resource_arns = [resource_arn]
+      self
+    end
+
     def with_sse(sse)
       @sse = sse
       @context_entries[:sse] = Aws::IAM::Types::ContextEntry.new({context_key_name: "s3:x-amz-server-side-encryption", context_key_values: [sse], context_key_type: "string"})
