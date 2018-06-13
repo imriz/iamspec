df512956ae5fb87d6b620d666ddea1b2384dfc68
Imri Zvik <imrizvik@gmail.com>
2018-06-13 14:23:34 +0300

    * Increase the retry limit. In the current settings, this means max of 68 minutes more or less

--- lib/iamspec.rb
+++ lib/iamspec.rb
@@ -10,7 +10,7 @@ require 'iamspec/action/s3_action'
 require 'iamspec/action/assume_role'
 
 Aws.config.update({
-                      retry_limit: 12
+                      retry_limit: 13
                   })
 
 extend Iamspec::Type
